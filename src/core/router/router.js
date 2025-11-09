47911e68937a56a8b544db01f1bb8c4cc3e02bf6
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-11-09 20:15:51 +0300

    добавил базовый роутинг

---
+++ src/core/router/router.js
@@ -0,0 +1,35 @@
+import { ROUTES } from './routes.data';
+import { NotFound } from '@/components/screens/not-found/not-found.component';
+
+export class Router {
+  #routes;
+  #currentRoute;
+  constructor() {
+    this.#routes = ROUTES;
+    this.#currentRoute = null;
+
+    this.#handelRouteChange()
+  }
+
+  getCurrentPath() {
+    return window.location.pathname;
+  }
+
+  #handelRouteChange() {
+    const path = this.getCurrentPath() || '/';
+    let route = this.#routes.find((route) => route.path === path);
+    if (!route) {
+      route = {
+        component: NotFound,
+      };
+    }
+    this.#currentRoute = route;
+    this.render();
+  }
+
+  render() {
+    const component = new this.#currentRoute.component();
+
+    document.getElementById('app').innerHTML = component.render();
+  }
+}
