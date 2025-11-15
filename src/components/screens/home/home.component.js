31d3a378e59e8dc3163bda8bf68d12d397f447f7
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-11-15 21:36:00 +0300

    добавлен renderService

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,10 +1,13 @@
+import template from './home.template.html';
 import { BaseScreen } from '@/core/component/base-screen-component';
+import renderService from '@/core/services/render.service';
 
 export class Home extends BaseScreen {
   constructor() {
     super({ title: 'Home' });
   }
   render() {
+    const element = renderService.htmlToElement(template);
     return '<p>HOME</p>';
   }
 }
--- src/core/services/render.service.js
+++ src/core/services/render.service.js
@@ -1,3 +1,45 @@
-class RenderService{
-  
+import { ChildComponent } from '../component/child.component';
+
+class RenderService {
+  htmlToElement(html, components = [], styles) {
+    const template = document.createElement('template');
+    template.innerHTML = html.trim();
+
+    const element = template.content.firstChild;
+    //styles
+
+    this.#replaceComponentTags(element, components);
+    return element;
+  }
+
+  /**
+   * @param {HTMLElement} parentElement
+   * @param {Array} components
+   */
+  #replaceComponentTags() {
+    const componentTagPattern = /^component-/;
+    const allElement = parentElement.getElementByTagName('*');
+
+    for (const element of allElement) {
+      const elementTagName = element.tagName.toLowerCase();
+      if (componentTagPattern.test(elementTagName)) {
+        const componentName = elementTagName.replace(componentTagPattern, '').replace(/-/g, '');
+
+        const foundComponent = components.find((component) => {
+          const instance = component instanceof ChildComponent ? component : new component();
+          return instance.constructor.name.toLowerCase() === componentName;
+        });
+
+        if (foundComponent) {
+          const componentContent =
+            foundComponent instanceof ChildComponent ? foundComponent.render() : new foundComponent.render();
+          element.replaceWith(componentContent);
+        } else {
+          console.error(`Component ${componentName} not found`);
+        }
+      }
+    }
+  }
 }
+
+export default new RenderService();
